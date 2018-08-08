93f4c9888e772ae4ebc02568e127c3834d56f6a6
KyleWang <wwenbin002@163.com>
2018-08-08 10:19:50 +0800

    Initial commit

---
+++ utilskit/src/main/java/com/kylewang/utils/utilskit/JsonUtil.java
@@ -0,0 +1,105 @@
+package com.kylewang.utils.utilskit;
+
+
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author KyleWang
+ * @email kylewangwenbin@163.com
+ * @date on 2018/4/4 0004 下午 5:11
+ * 将返回的json数据转成map  解决返回json为   "66":"示例"的问题
+ */
+
+public class JsonUtil {
+
+
+    /**
+     * 获取JsonObject
+     * @param json
+     * @return
+     */
+    public static JsonObject parseJson(String json){
+        JsonParser parser = new JsonParser();
+        JsonObject jsonObj = parser.parse(json).getAsJsonObject();
+        return jsonObj;
+    }
+
+    /**
+     * 根据json字符串返回Map对象
+     * @param json
+     * @return
+     */
+    public static Map<String,Object> toMap(String json){
+        return JsonUtil.toMap(JsonUtil.parseJson(json));
+    }
+
+    /**
+     * 将JSONObjec对象转换成Map-List集合
+     * @param json
+     * @return
+     */
+    public static Map<String, Object> toMap(JsonObject json){
+        Map<String, Object> map = new HashMap<String, Object>();
+        Set<Map.Entry<String, JsonElement>> entrySet = json.entrySet();
+        for (Iterator<Map.Entry<String, JsonElement>> iter = entrySet.iterator(); iter.hasNext(); ){
+            Map.Entry<String, JsonElement> entry = iter.next();
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            if(value instanceof JsonArray)
+                map.put((String) key, toList((JsonArray) value));
+            else if(value instanceof JsonObject)
+                map.put((String) key, toMap((JsonObject) value));
+            else
+                map.put((String) key, value);
+        }
+        return map;
+    }
+
+    /**
+     * 将JSONArray对象转换成List集合
+     * @param json
+     * @return
+     */
+    public static List<Object> toList(JsonArray json){
+        List<Object> list = new ArrayList<Object>();
+        for (int i=0; i<json.size(); i++){
+            Object value = json.get(i);
+            if(value instanceof JsonArray){
+                list.add(toList((JsonArray) value));
+            }
+            else if(value instanceof JsonObject){
+                list.add(toMap((JsonObject) value));
+            }
+            else{
+                list.add(value);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 将List 集合转换成 json
+     */
+    public static String toJsonString(List<HashMap<String,String>> hashMapList){
+        String jsonString = new Gson().toJson(hashMapList);
+        return jsonString;
+    }
+
+    public static String toJsonString(Object hashMap){
+        String jsonString  =new Gson().toJson(hashMap);
+        return jsonString;
+    }
+
+}
